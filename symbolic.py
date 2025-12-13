9735cca1af911cd9891ce95bb58db6f4f83e630a
Gaurav Manek <gaurav@gauravmanek.com>
2025-12-14 00:29:51 +0800

    Set up for Claude to solve.

--- symbolic.py
+++ symbolic.py
@@ -3,11 +3,22 @@
 
 from typing import Any
 
-from datamodel import Position
+from datamodel import Position, RobotModel, RobotPosition, RobotState
 
 
 class IKSymbolic:
-    """Implements a symbolic solver for inverse kinematics, making ."""
+    """Implements a symbolic solver for inverse kinematics using the Sympy solver."""
 
-    def __call__(self, desired: Position) -> Any:
+    def __init__(self, model: RobotModel) -> None:
+        # Create variables for each joint rotation and set up a system of equations that
+        # relate each end effector position
+        self.model = model
+
+    def __call__(self, state: RobotState) -> RobotPosition:
+        # Sanity-check that state.model is the same as self.model
+        # Given the desired end effector position:
+        state.desired_end_effector
+        # Use the sympy equations prepared in init to find joint angles that achieve
+        # the requested position. If the position is not possible, find the closest
+        # legal position.
         pass
